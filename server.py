37246e9feabcfb1ded38fb6e950b6c57d0c08c26
Subhash Pavan Chakravarthy Somarouthu <sspchakravarthy@gmail.com>
2024-07-28 00:18:31 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, render_template, request
+import util
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('bank_churn_data.html')
+
+@app.route('/predict_churn', methods=['GET','POST'])
+
+def predict():
+    data = request.get_json()
+    res = util.predict(data)
+    return str(res)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
